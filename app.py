63ed983ee977d7b5e5a58a591e549140efe16d33
Erik Schauer <do1ffe@darc.de>
2025-06-17 20:34:36 +0200

    Revert "Merge pull request #183 from DO1FFE/codex/daten-für-supercharger-abrufen"
    This reverts commit 449d3d54210ba00fe523017e41cc1548b8a27f3a, reversing
    changes made to 1cdc7eb172ef2f1455bd6204eec72dd7eb43f530.

--- app.py
+++ app.py
@@ -760,12 +760,6 @@ def _fetch_data_once(vehicle_id='default'):
         else:
             data = {'state': state}
 
-    if isinstance(data, dict):
-        try:
-            data['superchargers'] = get_superchargers(vid)
-        except Exception:
-            pass
-
     latest_data[cache_id] = data
     if isinstance(data, dict):
         try:
