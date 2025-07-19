f169874d646416f42bfebbba9f126055813662d9
Erik Schauer <do1ffe@darc.de>
2025-07-19 21:15:52 +0200

    Show receipt when taximeter stops

--- static/js/taxameter.js
+++ static/js/taxameter.js
@@ -22,7 +22,35 @@ $(function() {
         });
     }
 
+    function showReceipt(breakdown) {
+        if (!breakdown) {
+            return;
+        }
+        var lines = [];
+        lines.push('Grundpreis: ' + breakdown.base.toFixed(2) + ' \xE2\x82\xAC');
+        if (breakdown.km_1_2 > 0) {
+            lines.push(breakdown.km_1_2.toFixed(2) + ' km x ' +
+                breakdown.rate_1_2.toFixed(2) + ' \xE2\x82\xAC = ' +
+                breakdown.cost_1_2.toFixed(2) + ' \xE2\x82\xAC');
+        }
+        if (breakdown.km_3_4 > 0) {
+            lines.push(breakdown.km_3_4.toFixed(2) + ' km x ' +
+                breakdown.rate_3_4.toFixed(2) + ' \xE2\x82\xAC = ' +
+                breakdown.cost_3_4.toFixed(2) + ' \xE2\x82\xAC');
+        }
+        if (breakdown.km_5_plus > 0) {
+            lines.push(breakdown.km_5_plus.toFixed(2) + ' km x ' +
+                breakdown.rate_5_plus.toFixed(2) + ' \xE2\x82\xAC = ' +
+                breakdown.cost_5_plus.toFixed(2) + ' \xE2\x82\xAC');
+        }
+        lines.push('--------------------');
+        lines.push('Gesamt: ' + breakdown.total.toFixed(2) + ' \xE2\x82\xAC');
+        $('#receipt-text').text(lines.join('\n'));
+        $('#taximeter-receipt').show();
+    }
+
     $('#start-btn').click(function() {
+        $('#taximeter-receipt').hide();
         $.post('/api/taxameter/start', update, 'json');
     });
 
@@ -32,6 +60,7 @@ $(function() {
                 $('#price').text(Number(data.price).toFixed(2));
                 $('#dist').text(Number(data.distance).toFixed(2));
                 $('#time').text(Math.round(data.duration));
+                showReceipt(data.breakdown);
             }
             update();
         }, 'json');
@@ -42,6 +71,7 @@ $(function() {
         $('#price').text('0.00');
         $('#dist').text('0.00');
         $('#time').text('0');
+        $('#taximeter-receipt').hide();
     });
 
     update();
